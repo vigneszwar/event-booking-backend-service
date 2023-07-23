14400897728c3de2a4e57fc01bf0bacd1533720c
Adhiv <adhibignesh@gmail.com>
2023-07-23 15:39:33 +0530

    Add

--- src/main/java/com/TeamB/demo/model/Event.java
+++ src/main/java/com/TeamB/demo/model/Event.java
@@ -1,5 +1,17 @@
 package com.TeamB.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event
 {
     int Id;
