70136de62f2621d1d07c1c4595a7cedc55b5a240
Suriya-24 <suriyakarthick543@gmail.com>
2023-07-23 15:39:00 +0530

    added dependecies in hall by suriya

--- src/main/java/com/TeamB/demo/model/hall.java
+++ src/main/java/com/TeamB/demo/model/hall.java
@@ -1,5 +1,16 @@
 package com.TeamB.demo.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class hall
 {
     String hall_id,hall_type,hall_requirements;
